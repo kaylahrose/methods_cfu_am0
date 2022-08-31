1751d97798d69ee832ec485e3a8fd12af5f89b9d
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2022-08-31 13:47:39 -0700

    Complete challenge problems

--- define_and_id.rb
+++ define_and_id.rb
@@ -9,19 +9,31 @@
 
 # YOU DO the rest:
 
+# The include? method is called on the string object 'Hello World'
+# The argument 'Hello' is passed. include? checks if the argument exists
+# in the object and returns a boolean.
+# Returns 'True'
+p "Hello World".include?("Hello")
 
-"Hello World".include?("Hello")
+# the end_with? Method is called on the string object 'Hello World'.
+# The argument 'Hello' is passed. end_with? checks if the argument is at the end
+# of the object and returns a boolean value.
+# Returns 'False'
+p "Hello World".end_with?("Hello")
 
+# the end_with? Method is called on the string object 'Hello World'.
+# The argument 'rld' is passed. end_with? checks if the argument is at the end
+# of the object and returns a boolean value.
+# returns 'True'.
+p "Hello World".end_with?("rld")
 
-"Hello World".end_with?("Hello")
+# The even? method is called on the integer object 12.
+# The argument 12 is passed. even? checks if the argument is an even number and
+# returns a boolean value.
+# returns 'true'.
+p 12.even?
 
-
-"Hello World".end_with?("rld")
-
-
-12.even?
-
-
-18.next
-
-
+# the next method is called on the integer object 18.
+# The argument 18 is passed. next returns the following integer.
+# Returns '19'.
+p 18.next
--- methods_variables.rb
+++ methods_variables.rb
@@ -2,12 +2,19 @@
 # PART 1
 
 # Define a variable that stores a string
+name = 'Kaylah'
 
 #  call upcase on the variable, print it out
-#  call downcase on the variable, print it out
-#  call reverse on the variable, print it out
-#  call length on the variable, print it out
+p name.upcase
 
+#  call downcase on the variable, print it out
+p name.downcase
+
+#  call reverse on the variable, print it out
+p name.reverse
+
+#  call length on the variable, print it out
+p name.length
 
 
 
@@ -20,13 +27,49 @@
 user_name = "coco_11am"
 last_login = "12/09/2021"
 
-# 1: In the Ruby Docs for String methods (https://ruby-doc.org/core-3.1.0/String.html), find 4 methods you have not yet used and call them on one of the variables above. Between reading the documentation and reading the output from calling the methods, make sense of what they do. 
-  # Write the methods out on the lines below, with an explanation in your own words of how they work.
+# 1: In the Ruby Docs for String methods (https://ruby-doc.org/core-3.1.0/String.html),
+# find 4 methods you have not yet used and call them on one of the variables above.
+# Between reading the documentation and reading the output from calling the methods,
+# make sense of what they do.
+# Write the methods out on the lines below, with an explanation in your own words
+# of how they work.
+
+# The method squeeze calls the string object user_name. squeeze removes duplicate characters
+# from the object. It can take an argument, range, or no argument.
+# Returns 'coco_1am'.
+user_name.squeeze('1')
+p user_name
+
+# The method prepend calls the string object user_name. prepend prepends the String
+# with the argument(s).
+# Returns 'hot_coco_11am'
+p user_name.prepend('hot_')
+
+# The method bytesize calls the string object last_login. bytesize returns the
+# count of bytes in the object.
+# returns '10'
+p last_login.bytesize
+
+# The clear method calls the string object last_login. clear removes the contents
+# of the object.
+# Returns "".
+p last_login.clear
+
+# 2: Do some research (either testing out with your own code or Googling) to
+# build an understanding of what the `!` does, when at the end of a method name.
+# Show your understading by providing an example and writing an explanation.
+
+# The bang '!' method modifies the object it calls, rather than copying the object.
+# this is considered a dangerous method.
+
+p user_name
+p user_name.upcase
+# prints a copy of the variable with method applied.
 
 
-
-# 2: Do some research (either testing out with your own code or Googling) to build an understanding of what the `!` does, when at the end of a method name. Show your understading by providing an example and writing an explanation.
-
-
-
-
+p user_name
+# prints unchanged variable
+p user_name.upcase!
+# modifies the variable and prints result
+p user_name
+# prints the modified variable with no methods calling it.
