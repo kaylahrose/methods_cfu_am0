ca0bcf8b168f309ffe198d1b71125159d3921b3b
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2022-08-31 14:30:01 -0700

    Complete practice problems

--- final_practice.rb
+++ final_practice.rb
@@ -1,38 +1,67 @@
 # Defining Our Own Methods
 # 1: Write a method named greeting that prints out a general greeting to someone
+def greeting
+  return 'Hi, Friend!'
+end
+p greeting
+
 
 # What is the return value of your method?
-# How many arguments did you pass your method?
+# 'Hi, Friend!'
 
+# How many arguments did you pass your method?
+# no arguments
 
 
 
 #2: Write a method named custom_greeting that prints out a greeting WITH a specific name.
+def custom_greeting(name)
+  return "Hi, #{name}!"
+end
+p custom_greeting("Kaylah")
+# What is the return value of your method?
+# "Hi, Kaylah"
+
+# How many arguments did you pass your method?
+# 1
+
+# What data type was your argument(s)?
+# String
+
+
+
+#3: Write a method named square that takes in one number, and returns the square
+# of that number
+def square(num)
+  return num * num
+end
+p square(12)
 
 
 # What is the return value of your method?
+# 144
+
 # How many arguments did you pass your method?
+# 1
+
 # What data type was your argument(s)?
+# integer but can work for float
 
 
 
 
-#3: Write a method named square that takes in one number, and returns the square of that number
-
-
+#4: Write a method named greet_person that takes in 3 strings, a first, middle,
+# and last name, and print outs the sentence of the entire string
+def greet_person(first, middle, last)
+  return "Hi, #{first} #{middle} #{last}"
+end
+p greet_person("Kaylah", 'Rose', 'Mitchell')
 
 # What is the return value of your method?
+# 'Hi, Kaylah Rose Mitchell'
+
 # How many arguments did you pass your method?
+# 3 arguments
+
 # What data type was your argument(s)?
-
-
-
-
-#4: Write a method named greet_person that takes in 3 strings, a first, middle, and last name, and print outs the sentence of the entire string
-
-
-
-# What is the return value of your method?
-# How many arguments did you pass your method?
-# What data type was your argument(s)?
-
+# all strings
